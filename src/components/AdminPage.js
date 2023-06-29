6d52f4e2416a452f21fee9c95c9f33f73ff00134
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-06-29 15:27:05 +0530

    Minor style changes.

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -19,18 +19,18 @@ const AdminPage = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+            <li className="nav-item me-2">
               <a className="nav-link" href="/StudentOperations">
                 Student Operations
               </a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item me-2">
               <a className="nav-link" href="/InstructorOperations">
                 Instructor Operations
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a className="nav-link" href="/" id = "logOut">
                 Log Out
               </a>
             </li>
